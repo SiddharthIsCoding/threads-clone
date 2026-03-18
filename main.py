9fa6b7b364023ad1f0af70e28783a6ac95802389
siddbytheway <88389185+SiddharthIsCoding@users.noreply.github.com>
2026-03-18 12:46:25 +0530

    routing

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-import flask
+import flask , mysql.connector
 from flask import *
+
+
 
 myapp = Flask(__name__)
 
 
-@app.route("/home")
+@myapp.route("/home")
 def home():
     return render_template("home.html")
 
+@myapp.route("/login")
+def login():
+    return render_template("login.html")
 
+@myapp.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+if __name__ == "__main__":
+    myapp.run(debug=True)
