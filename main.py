61c51ba0b4accff39557a5b57fb6e4700dea6762
siddbytheway <88389185+SiddharthIsCoding@users.noreply.github.com>
2026-03-19 17:39:16 +0530

    fixing the form

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import flask , mysql.connector
+import flask , mysql.connector , random
+
 from flask import *
 
+myconn = mysql.connector.connect(host='localhost',user='root',passwd='root',db="threadsbase")
 
+mycur = myconn.cursor()
+
+print("db connection : " ,myconn)
 
 myapp = Flask(__name__)
 
@@ -17,6 +22,30 @@ def login():
 @myapp.route("/signup")
 def signup():
     return render_template("signup.html")
+
+@myapp.route("/submit", methods=['POST'])
+def submit():
+    print("bitch look at me")
+    uid = random.randint(0,1000)
+    username = request.form.get("username")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    name = request.form.get("name")
+
+    try:
+        query = "INSERT INTO users VALUES (%s, %s, %s, %s, %s)"
+        values = (uid, username, password, email, name)
+
+        mycur.execute(query, values)
+        myconn.commit()
+
+        print("inserted data", uid, username, email, password, name)
+        return "Signup successful"
+
+    except Exception as e:
+        print("Error:", e)
+        return "Signup failed"
+    
 
 
 if __name__ == "__main__":
